e1636bef2f909e135efee8b01f77493c73b2299b
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-07-28 12:29:32 +0900

    Render it to the device

--- index.js
+++ index.js
@@ -6,3 +6,6 @@ import { Text, AppRegistry } from 'react-native';
 const App = () => (
     <Text>Some Text</Text>
   );
+
+// Render it to the device
+AppRegistry.registerComponent('albums', () => App);
