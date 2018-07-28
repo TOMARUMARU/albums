00d67d61f59a738d2a02bfd93fb2d25c9c4f22a9
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-07-28 12:29:06 +0900

    // Import a library to help create a component

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-/** @format */
-
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+// Import a library to help create a component
+import React from 'react';
+import { Text, AppRegistry } from 'react-native';
