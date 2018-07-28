16768afb218da7bc038998d4e51c858525cc5eba
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-07-28 12:29:21 +0900

    Create a component

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 // Import a library to help create a component
 import React from 'react';
 import { Text, AppRegistry } from 'react-native';
+
+// Create a component
+const App = () => (
+    <Text>Some Text</Text>
+  );
