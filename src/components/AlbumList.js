730ee2e5175e5c8eb02ee5d9168285d2285f6544
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-09 18:29:41 +0900

    import axios

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import axions from 'axios';
 
 class AlbumList extends Component {
+  componentWillMount() {
+    console.log('test');
+  }
+
   render() {
     return (
       <View>
