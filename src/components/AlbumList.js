64047ae69ed91134c4fb7e57d75010ffaa95afcd
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-09 15:29:44 +0900

    create AlbunList.js

---
+++ src/components/AlbumList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+impot { View, Text } from 'react-native';
+
+const AlbumList = () => {
+  return (
+    <View>
+      <Text>Album List!!!</Text>
+    </View>
+  );
+};
+
+export default AlbumList;
