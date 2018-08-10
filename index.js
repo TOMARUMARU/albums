d4881bf408134c62bc4c636f60d92ba084d328ba
SHOKI TOMARU <ac.milan.22x36@gmail.com>
2018-08-10 18:40:49 +0900

    remove warning

--- index.js
+++ index.js
@@ -14,3 +14,7 @@ const App = () => (
 
 // Render it to the device
 AppRegistry.registerComponent('albums', () => App);
+
+console.ignoredYellowBox = [
+  'Remote debugger is in a background tab which may cause apps to perform slowly. Fix this by foregrounding the tab (or opening it in a separate window).',
+];
